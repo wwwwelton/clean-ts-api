7f9292fca22200b162193c5bee8f2cc1e26d5715
avatar9966 <av4t@hotmail.com>
2021-01-26 17:00:30 -0300

    refactor: move required validation to the ValidationComposite

---
+++ src/main/factories/signup-validation.ts
@@ -0,0 +1,11 @@
+import { RequiredFieldValidation } from '../../presentation/helpers/validators/required-field-validation'
+import { ValidationComposite } from '../../presentation/helpers/validators/validation-composite'
+import { Validation } from '../../presentation/helpers/validators/validation'
+
+export const makeSignUpValidation = (): ValidationComposite => {
+  const validations: Validation[] = []
+  for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
+    validations.push(new RequiredFieldValidation(field))
+  }
+  return new ValidationComposite(validations)
+}
