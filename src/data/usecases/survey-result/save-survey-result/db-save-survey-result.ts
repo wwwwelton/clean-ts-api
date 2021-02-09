267ed40a5a5494886252fe5495e1f151d1d2eab2
avatar9966 <av4t@hotmail.com>
2021-02-09 20:51:49 -0300

    refactor: rename argument models sufix from Model to Params

--- src/data/usecases/survey-result/save-survey-result/db-save-survey-result.ts
+++ src/data/usecases/survey-result/save-survey-result/db-save-survey-result.ts
@@ -1,9 +1,9 @@
-import { SaveSurveyResult, SaveSurveyResultModel, SurveyResultModel, SaveSurveyResultRepository } from './db-save-survey-result-protocols'
+import { SaveSurveyResult, SaveSurveyResultParams, SurveyResultModel, SaveSurveyResultRepository } from './db-save-survey-result-protocols'
 
 export class DbSaveSurveyResult implements SaveSurveyResult {
   constructor (private readonly saveSurveyResultRepositoryStub: SaveSurveyResultRepository) { }
 
-  async save (data: SaveSurveyResultModel): Promise<SurveyResultModel> {
+  async save (data: SaveSurveyResultParams): Promise<SurveyResultModel> {
     const surveyResult = await this.saveSurveyResultRepositoryStub.save(data)
     return surveyResult
   }
--- src/presentation/controllers/login/login/login-controller.spec.ts
+++ src/presentation/controllers/login/login/login-controller.spec.ts
@@ -2,11 +2,11 @@ import { LoginController } from './login-controller'
 import { HttpRequest, Authentication, Validation } from './login-controller-protocols'
 import { badRequest, serverError, unauthorized, ok } from '@/presentation/helpers/http/http-helper'
 import { MissingParamError } from '@/presentation/errors'
-import { AuthenticationModel } from '@/domain/usescases/account/authentication'
+import { AuthenticationParams } from '@/domain/usescases/account/authentication'
 
 const makeAuthentication = (): Authentication => {
   class AuthenticationStub implements Authentication {
-    async auth (authentication: AuthenticationModel): Promise<string> {
+    async auth (authentication: AuthenticationParams): Promise<string> {
       return await new Promise(resolve => resolve('any_token'))
     }
   }
