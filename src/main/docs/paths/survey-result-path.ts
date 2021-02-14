113bff833da2a8bc4687e07a5e0a753d18157285
avatar9966 <av4t@hotmail.com>
2021-02-14 19:27:01 -0300

    docs: add load survey result path

--- src/main/docs/paths/survey-result-path.ts
+++ src/main/docs/paths/survey-result-path.ts
@@ -5,6 +5,7 @@ export const surveyResultPath = {
     }],
     tags: ['Enquete'],
     summary: 'API para criar a resposta de uma enquete',
+    description: 'Essa rota só pode ser executada por **usuários autenticados**',
     parameters: [{
       in: 'path',
       name: 'surveyId',
@@ -22,6 +23,43 @@ export const surveyResultPath = {
         }
       }
     },
+    responses: {
+      200: {
+        description: 'Sucesso',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/surveyResult'
+            }
+          }
+        }
+      },
+      403: {
+        $ref: '#/components/forbidden'
+      },
+      404: {
+        $ref: '#/components/notFound'
+      },
+      500: {
+        $ref: '#/components/serverError'
+      }
+    }
+  },
+  get: {
+    security: [{
+      apiKeyAuth: []
+    }],
+    tags: ['Enquete'],
+    summary: 'API para consultar o resultado de uma enquete',
+    description: 'Essa rota só pode ser executada por **usuários autenticados**',
+    parameters: [{
+      in: 'path',
+      name: 'surveyId',
+      required: true,
+      schema: {
+        type: 'string'
+      }
+    }],
     responses: {
       200: {
         description: 'Sucesso',
