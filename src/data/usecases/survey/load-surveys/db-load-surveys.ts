4d81dac64d4d40d219a5eb534e3841aabb421c3c
avatar9966 <av4t@hotmail.com>
2021-02-17 00:55:50 -0300

    feat: change load surveys to add didAnswer field

--- src/data/usecases/survey/load-surveys/db-load-surveys.ts
+++ src/data/usecases/survey/load-surveys/db-load-surveys.ts
@@ -1,10 +1,10 @@
 import { LoadSurveysRepository, LoadSurveys, SurveyModel } from './db-load-surveys-protocols'
 
 export class DbLoadSurveys implements LoadSurveys {
-  constructor (private readonly loadSurveysRepository: LoadSurveysRepository) {}
+  constructor (private readonly loadSurveysRepository: LoadSurveysRepository) { }
 
-  async load (): Promise<SurveyModel[]> {
-    const surveys = await this.loadSurveysRepository.loadAll()
+  async load (accountId: string): Promise<SurveyModel[]> {
+    const surveys = await this.loadSurveysRepository.loadAll(accountId)
     return surveys
   }
 }
--- src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
@@ -2,11 +2,11 @@ import { Controller, HttpRequest, HttpResponse, LoadSurveys } from './load-surve
 import { ok, serverError, noContent } from '@/presentation/helpers/http/http-helper'
 
 export class LoadSurveysController implements Controller {
-  constructor (private readonly loadSurveys: LoadSurveys) {}
+  constructor (private readonly loadSurveys: LoadSurveys) { }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      const surveys = await this.loadSurveys.load()
+      const surveys = await this.loadSurveys.load(httpRequest.accountId)
       return surveys.length ? ok(surveys) : noContent()
     } catch (error) {
       return serverError(error)
