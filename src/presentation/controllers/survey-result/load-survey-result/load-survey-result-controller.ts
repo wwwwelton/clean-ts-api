fbec6fb9395ff4228ba70bf03fe87838ed6570b6
avatar9966 <av4t@hotmail.com>
2021-02-17 19:47:13 -0300

    refactor: change SurveyResult to return isCurrentAccountAnswer

--- src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
@@ -6,7 +6,7 @@ export class LoadSurveyResultController implements Controller {
   constructor (
     private readonly loadSurveyById: LoadSurveyById,
     private readonly loadSurveyResult: LoadSurveyResult
-  ) {}
+  ) { }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
@@ -15,7 +15,7 @@ export class LoadSurveyResultController implements Controller {
       if (!survey) {
         return forbidden(new InvalidParamError('surveyId'))
       }
-      const surveyResult = await this.loadSurveyResult.load(surveyId)
+      const surveyResult = await this.loadSurveyResult.load(surveyId, httpRequest.accountId)
       return ok(surveyResult)
     } catch (error) {
       return serverError(error)
